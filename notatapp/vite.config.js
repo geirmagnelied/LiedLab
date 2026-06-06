1e64f0d4797540758df174a361b6de4dfbbf386d
Geir Magne Lied <geir.magne.lied@norconsult.com>
2026-06-06 09:57:26 +0200

    Fiksar base path for Vercel

--- notatapp/vite.config.js
+++ notatapp/vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/liedlab/notatblokk/',
-  build: {
-    outDir: 'dist',
-  },
+  base: './',
 })
